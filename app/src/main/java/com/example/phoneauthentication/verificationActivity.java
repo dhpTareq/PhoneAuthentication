ca621abea39c28add13e93170c90d03608e021f7
dhpTareq <tareq19991218@gmail.com>
2023-08-09 06:06:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/phoneauthentication/verificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.phoneauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
